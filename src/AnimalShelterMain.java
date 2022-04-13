4b1372a48403b4f9814cca38ca52bd53576d072a
Philipp Bomers <mail@philipp-bomers.de>
2022-04-13 08:19:28 +0200

    updated comparator

--- src/AnimalShelterMain.java
+++ src/AnimalShelterMain.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class AnimalShelterMain {
     public static void main(String[] args) {
@@ -10,13 +9,7 @@ public class AnimalShelterMain {
         animals.add(new Dog("Kymani", 2000));
         animals.add(new Dog("Ferdi", 1999));
         animals.add(new Cat("Mietzi", 3));
-        Comparator<AnimalInterface> comparator = new Comparator<AnimalInterface>() {
-            @Override
-            public int compare(AnimalInterface o1, AnimalInterface o2) {
-                return o1.getCuteness()-o2.getCuteness();
-            }
-        };
-        List<AnimalInterface> sortedAnimals = animals.stream().sorted(comparator).toList();
+        List<AnimalInterface> sortedAnimals = animals.stream().sorted(Comparator.comparingInt(AnimalInterface::getCuteness)).toList();
         System.out.println("The cutest animals ascending");
         sortedAnimals.forEach(entry -> System.out.println(entry.getName()));
     }
